ae28da14942e999efc241264b219eeca94a65fc8
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2020-08-29 11:53:20 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EightQueens test = new EightQueens();
+        System.out.println(test.solve());
+    }
+}
